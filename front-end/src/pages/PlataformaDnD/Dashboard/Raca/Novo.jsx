6f1f9e0140f60f84fd4107fbefe5f3db9f74212e
Diego Matos <diegovasconcelos62@gmail.com>
2020-07-03 18:41:18 -0300

    dashboard pt2
    passos de bebê kkkk

--- front-end/src/pages/PlataformaDnD/Dashboard/Raca/Novo.jsx
+++ front-end/src/pages/PlataformaDnD/Dashboard/Raca/Novo.jsx
@@ -1,22 +1,93 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
 export default class Novo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: "",
+      slug: "",
+      tipo: "Raça",
+    };
+  }
+
+  handleInputChange = (e) => {
+    const { name, value, type } = e.target;
+    if (type === "text") {
+      this.setState((prevState) => {
+        let newState = {};
+        if (name === "nome") {
+          let sugestaoDeSlug = value.trim().replace(" ", "-").toLowerCase();
+          newState.slug = sugestaoDeSlug;
+        }
+        newState[name] = value;
+        return newState;
+      });
+    }
+  };
+
   render() {
     return (
       <main>
-         <h1>Adicionar nova Raça</h1>
-         <hr/>
-         <div className="container">
-          <div className="container-formulario-imagem">
-            <form className="formulario-imagem">
+        <h1>Adicionar nova Raça</h1>
+        <hr />
+        <aside>
+          <div className="container-formulario-dnd">
+            <form className="formulario-dnd">
               <fieldset>
-                <legend>Adicionar Imagem da Raça</legend>
-                <input type="file" accept="image/jpeg, image/png, image/webp"/>
+                <legend>Dados da Raça</legend>
+                <label htmlFor="nome">Nome: </label>
+                <input
+                  id="nome"
+                  name="nome"
+                  placeholder="Ex: Alto Elfo"
+                  type="text"
+                  value={this.state.nome}
+                  onChange={this.handleInputChange}
+                />
+                <br />
+                <label htmlFor="slug">Slug: </label>
+                <input
+                  id="slug"
+                  name="slug"
+                  placeholder="Ex: anao-da-colina"
+                  type="text"
+                  value={this.state.slug}
+                  onChange={this.handleInputChange}
+                />
+                <br />
+                <label htmlFor="raca">Raça: </label>
+                <input
+                  readOnly
+                  id="raca"
+                  name="raca"
+                  type="text"
+                  value={this.state.tipo}
+                />
+                <br />
+                <label htmlFor="descricao">Descrição: </label>
+                <Editor
+                  initialValue="<p>This is the initial content of the editor</p>"
+                  init={{
+                    height: 500,
+                    menubar: false,
+                    plugins: [
+                      "advlist autolink lists link image charmap print preview anchor",
+                      "searchreplace visualblocks code fullscreen",
+                      "insertdatetime media table paste code help wordcount",
+                    ],
+                    toolbar:
+                      "undo redo | formatselect | bold italic backcolor | \
+             alignleft aligncenter alignright alignjustify | \
+             bullist numlist outdent indent | removeformat | help",
+                  }}
+                  onEditorChange={this.handleEditorChange}
+                />
               </fieldset>
             </form>
           </div>
-         </div>
+        </aside>
       </main>
-    )
+    );
   }
 }
